5efe728f4f3ba88678bfad8491fc76c765df532f
muhammet ali safak <muhammet.ali.safak@gmail.com>
2024-10-06 23:26:59 +0300

    quiz-time
    quiz-time

---
+++ index.js
@@ -0,0 +1,77 @@
+
+let sayac = 0;
+
+let quiz1 = prompt("1) Hangi hayvanın 3 kalbi vardır?\n a)Köpek \n b)Ahtapot \n c)Balina \n d)At ");
+if(quiz1 === "b"){
+  sayac = sayac +1;
+} else if( quiz1 ==="a" || quiz1 ==="c" || quiz1 ==="d"){
+  alert("Cevabınız yanlış.")
+} else {alert("Yanlış tuşlama yaptınız")}
+
+let quiz2 = prompt("2) 'Harry Potter' serisinde, Harry'nin favori Quidditch pozisyonu hangisidir? \n a)Tutucu \n b)Golcü \n c)Arayıcı \n d)Vurucu ");
+if(quiz2 === "c"){
+  sayac = sayac +1;
+} else if( quiz2 ==="a" || quiz2 ==="b" || quiz2 ==="d"){
+  alert("Cevabınız yanlış.")
+} else{alert("Yanlış tuşlama yaptınız")}
+
+let quiz3 = prompt("3)  Dünyanın en yüksek dağı olan Everest'in yüksekliği yaklaşık kaç metredir? \n a)6,848 m \n b)7,234 m \n c)8,848 m \n d)9,142 m ");
+if(quiz3 === "c"){
+  sayac = sayac +1;
+} else if( quiz3 ==="a" || quiz3 ==="b" || quiz3 ==="d"){
+  alert("Cevabınız yanlış.")
+} else{alert("Yanlış tuşlama yaptınız")}
+
+let quiz4 = prompt("4) Hangi ülke, çikolatanın ilk kez üretildiği yer olarak kabul edilir?\n a)İsviçre \n b)Belçika \n c)İtalya \n d)Meksika ");
+if(quiz4 === "d"){
+  sayac = sayac +1;
+} else if( quiz4 ==="a" || quiz4 ==="c" || quiz4 ==="b"){
+  alert("Cevabınız yanlış.")
+} else{alert("Yanlış tuşlama yaptınız")}
+
+let quiz5 = prompt("5) Hangisi 'Matrix' film serisinde kullanılan meşhur mavi ve kırmızı hap sahnesinde söylenen repliklerden biridir?\n a)Zaman yolculuğu seni bekliyor. \n b)Kırmızı hapı seç, gerçeği gör. \n c)Mavi hapı seç, uyanırsın. \n d)Gerçek sandığından daha karmaşık. ");
+if(quiz5 === "b"){
+  sayac = sayac +1;
+} else if( quiz5 ==="a" || quiz5 ==="c" || quiz5 ==="d"){
+  alert("Cevabınız yanlış.")
+} else{alert("Yanlış tuşlama yaptınız")}
+
+let quiz6 = prompt("6) Aşağıdakilerden hangisi insan beynindeki en büyük kısımdır?\n a)Beyincik \n b)Hipotalamus \n c)Beyin Kabuğu \n d)Omurilik ");
+if(quiz6 === "c"){
+  sayac = sayac +1;
+} else if( quiz6 ==="a" || quiz6 ==="b" || quiz6 ==="d"){
+  alert("Cevabınız yanlış.")
+} else{alert("Yanlış tuşlama yaptınız")}
+
+let quiz7 = prompt("7) 'Simpsons' çizgi filminde, Bart Simpson’ın en sevdiği slogan hangisidir?\n a)Ayakkabılarım nerede? \n b)Yapma bunu, adamım! \n c)Ay canına! \n d)Ben yapmadım! ");
+if(quiz7 === "d"){
+  sayac = sayac +1;
+} else if( quiz7 ==="a" || quiz7 ==="c" || quiz7 ==="b"){
+  alert("Cevabınız yanlış.")
+} else{alert("Yanlış tuşlama yaptınız")}
+
+let quiz8 = prompt("8) Hangisi denizde yaşayan bir bitki türüdür?\n a)Mercan \n b)Yosun \n c)Midye \n d)Denizanası ");
+if(quiz8 === "b"){
+  sayac = sayac +1;
+} else if( quiz8 ==="a" || quiz8 ==="c" || quiz8 ==="d"){
+  alert("Cevabınız yanlış.")
+} else{alert("Yanlış tuşlama yaptınız")}
+
+let quiz9 = prompt("9) Hangi elementin simgesi 'Fe' dir?\n a)Flor \n b)Demir \n c)Kurşun \n d)Fosfor ");
+if(quiz9 === "b"){
+  sayac = sayac +1;
+} else if( quiz9 ==="a" || quiz9 ==="c" || quiz9 ==="d"){
+  alert("Cevabınız yanlış.")
+} else{alert("Yanlış tuşlama yaptınız")}
+
+let quiz10 = prompt("10) 2024'te yapılacak Olimpiyat Oyunları hangi şehirde düzenlendi ?\n a)Tokyo \n b)Los Angeles \n c)Paris \n d)Rio de Janeiro ");
+if(quiz10 === "c"){
+  sayac = sayac +1;
+} else if( quiz10 ==="a" || quiz10 ==="b" || quiz10 ==="d"){
+  alert("Cevabınız yanlış.")
+} else{alert("Yanlış tuşlama yaptınız")}
+
+alert("Doğru yaptıgınız soru sayısı: " + sayac + " tebrik ederiz.")
+
+
+
